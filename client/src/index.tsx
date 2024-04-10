66425dfdc90dd994ab9cc040300ff017216b94b0
ilesoviy <ilisovij861@gmail.com>
2024-04-11 02:57:12 +0300

    Copied entire code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
+
+const wallets = [new PetraWallet()];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+      <App />
+    </AptosWalletAdapterProvider>
   </React.StrictMode>
 );
 

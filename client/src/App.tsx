c4d033c9776e3b4f1847dcc617874019edecb896
ilesoviy <ilisovij861@gmail.com>
2024-04-10 19:28:09 +0300

    Configured initial UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Layout, Row, Col } from "antd";
 
 function App() {
-  return <div>My app goes here</div>;
-}
+  return (
+    <>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <h1>Connect Wallet</h1>
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  )
+};
 
 export default App;
